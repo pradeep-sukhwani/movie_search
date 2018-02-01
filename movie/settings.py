e416ea4cc7f44c9957ac666e256b849bb63dc320
ps.sukhwani <ps.sukhwani@gmail.com>
2018-02-01 10:19:22 +0530

    diabled collectstatic

--- movie/settings.py
+++ movie/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 SHELL = "ipython"
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # STATIC_ROOT = '/home/family/'
 
 STATICFILES_DIRS = ["static"]
